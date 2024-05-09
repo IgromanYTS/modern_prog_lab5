a09f1f0d2da2ea146abb2c6b548fa8a7b1c287c0
IgromanYTS <83410566+IgromanYTS@users.noreply.github.com>
2024-05-09 21:49:02 +0300

    Update test_calc.py

--- test_calc.py
+++ test_calc.py
@@ -1,33 +1,7 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.prev = None
-        self.next = None
+from calc import add, subtract, multiply, division
+import pytest
 
 
-def print_words(word_list):
-    if len(word_list) == 0:
-        return
-
-    
-    def is_odd_length(word):
-        return len(word) % 2 != 0
-
-    
-    head = Node(None)  
-    current = head
-    for word in word_list:
-        if is_odd_length(word):
-            word_trimmed = word[1:-1] + ","
-            current.next = Node(word_trimmed)
-            current.next.prev = current
-            current = current.next
-
-    
-    current = head.next  
-    while current is not None:
-        print(current.data)
-        current = current.next
-
-words = ["apple", "banana", "orange"]
-print_words(words)
+def test_add():
+    assert add(2, 4) == 6
+    assert add("Dima", "cool") == "Dimacool"
